881cfbf5eda8dcae4e3de7161c1f61947904146d
coffeelover42 <git@git.com>
2026-07-07 20:26:11 -0400

    Setup

--- src/main/java/rosesmp/roseranks/RoseRanks.java
+++ src/main/java/rosesmp/roseranks/RoseRanks.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package rosesmp.roseranks;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -7,13 +7,13 @@ import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-	public static final String MOD_ID = HalpLibe.registerMod("examplemod", true);
+public class RoseRanks implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+	public static final String MOD_ID = HalpLibe.registerMod("roseranks", true);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("Rose Ranks initialized.");
 	}
 
 	@Override
